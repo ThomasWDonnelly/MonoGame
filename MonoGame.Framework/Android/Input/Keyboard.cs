d37d0060ca3b2febcb7ddc4785eee98c2cfa16ed
unknown <bgearon@grasshoppah.(none)>
2012-05-11 23:33:06 -0500

    Added keys

--- MonoGame.Framework/Android/Input/Keyboard.cs
+++ MonoGame.Framework/Android/Input/Keyboard.cs
@@ -77,7 +77,99 @@ namespace Microsoft.Xna.Framework.Input
             maps[Keycode.DpadUp] = Keys.Up;
             maps[Keycode.DpadDown] = Keys.Down;
 			maps[Keycode.DpadCenter] = Keys.Enter;
-			maps[Keycode.A] = Keys.A;
+
+            maps[Keycode.Back] = Keys.Back;
+            maps[Keycode.Tab] = Keys.Tab;
+            maps[Keycode.Enter] = Keys.Enter;
+            maps[Keycode.Space] = Keys.Space;
+            maps[Keycode.Home] = Keys.Home;
+            maps[Keycode.Enter] = Keys.Select;
+            maps[Keycode.Num0] = Keys.D0;
+            maps[Keycode.Num1] = Keys.D1;
+            maps[Keycode.Num2] = Keys.D2;
+            maps[Keycode.Num3] = Keys.D3;
+            maps[Keycode.Num4] = Keys.D4;
+            maps[Keycode.Num5] = Keys.D5;
+            maps[Keycode.Num6] = Keys.D6;
+            maps[Keycode.Num7] = Keys.D7;
+            maps[Keycode.Num8] = Keys.D8;
+            maps[Keycode.Num9] = Keys.D9;
+            maps[Keycode.A] = Keys.A;
+            maps[Keycode.B] = Keys.B;
+            maps[Keycode.C] = Keys.C;
+            maps[Keycode.D] = Keys.D;
+            maps[Keycode.E] = Keys.E;
+            maps[Keycode.F] = Keys.F;
+            maps[Keycode.G] = Keys.G;
+            maps[Keycode.H] = Keys.H;
+            maps[Keycode.I] = Keys.I;
+            maps[Keycode.J] = Keys.J;
+            maps[Keycode.K] = Keys.K;
+            maps[Keycode.L] = Keys.L;
+            maps[Keycode.M] = Keys.M;
+            maps[Keycode.N] = Keys.N;
+            maps[Keycode.O] = Keys.O;
+            maps[Keycode.P] = Keys.P;
+            maps[Keycode.Q] = Keys.Q;
+            maps[Keycode.R] = Keys.R;
+            maps[Keycode.S] = Keys.S;
+            maps[Keycode.T] = Keys.T;
+            maps[Keycode.U] = Keys.U;
+            maps[Keycode.V] = Keys.V;
+            maps[Keycode.W] = Keys.W;
+            maps[Keycode.X] = Keys.X;
+            maps[Keycode.Y] = Keys.Y;
+            maps[Keycode.Z] = Keys.Z;
+            maps[Keycode.Home] = Keys.LeftWindows;
+            maps[Keycode.Home] = Keys.RightWindows;
+            maps[Keycode.Home] = Keys.Apps;
+            maps[Keycode.Power] = Keys.Sleep;
+            maps[Keycode.Num0] = Keys.NumPad0;
+            maps[Keycode.Num1] = Keys.NumPad1;
+            maps[Keycode.Num2] = Keys.NumPad2;
+            maps[Keycode.Num3] = Keys.NumPad3;
+            maps[Keycode.Num4] = Keys.NumPad4;
+            maps[Keycode.Num5] = Keys.NumPad5;
+            maps[Keycode.Num6] = Keys.NumPad6;
+            maps[Keycode.Num7] = Keys.NumPad7;
+            maps[Keycode.Num8] = Keys.NumPad8;
+            maps[Keycode.Num9] = Keys.NumPad9;
+            maps[Keycode.Star] = Keys.Multiply;
+            maps[Keycode.Plus] = Keys.Add;
+            
+            maps[Keycode.Minus] = Keys.Subtract;
+            maps[Keycode.Period] = Keys.Decimal;
+            maps[Keycode.Slash] = Keys.Divide;
+            
+            maps[Keycode.AltLeft] = Keys.LeftAlt;
+            maps[Keycode.AltRight] = Keys.RightAlt;
+            
+            maps[Keycode.VolumeDown] = Keys.VolumeDown;
+            maps[Keycode.VolumeUp] = Keys.VolumeUp;
+            maps[Keycode.MediaNext] = Keys.MediaNextTrack;
+            maps[Keycode.MediaPrevious] = Keys.MediaPreviousTrack;
+            maps[Keycode.MediaStop] = Keys.MediaStop;
+            maps[Keycode.MediaPlayPause] = Keys.MediaPlayPause;
+
+            maps[Keycode.Semicolon] = Keys.OemSemicolon;
+            maps[Keycode.Plus] = Keys.OemPlus;
+            maps[Keycode.Comma] = Keys.OemComma;
+            maps[Keycode.Minus] = Keys.OemMinus;
+            maps[Keycode.Period] = Keys.OemPeriod;
+            maps[Keycode.Search] = Keys.OemQuestion;
+            maps[Keycode.Grave] = Keys.OemTilde;
+            maps[Keycode.LeftBracket] = Keys.OemOpenBrackets;
+            maps[Keycode.RightBracket] = Keys.OemCloseBrackets;
+            maps[Keycode.Backslash] = Keys.OemBackslash;
+            
+            maps[Keycode.MediaPlayPause] = Keys.Play;
+            
+            
+            maps[Keycode.Clear] = Keys.OemClear;
+            
+            maps[Keycode.MediaPlayPause] = Keys.Pause;
+            
+            maps[Keycode.A] = Keys.A;
 			maps[Keycode.Y] = Keys.Y;
 			maps[Keycode.X] = Keys.X;
 			maps[Keycode.B] = Keys.B;
